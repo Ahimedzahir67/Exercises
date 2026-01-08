13dd3575c305dec3e85694501b006c0eb9754808
Ahimedzahir67 <kerons1234@gmail.com>
2026-01-08 03:43:29 +0300

    complet Hello-09
    basic function structure and

---
+++ Hello-09.py
@@ -0,0 +1,11 @@
+# Exercise 9: Hello
+def hello():
+    # Fill in this blank to print "Hello" to the console
+    print("Hello") 
+
+def main():
+    # Fill in this blank to call the hello() function
+    hello() 
+
+if __name__ == "__main__":
+    main()

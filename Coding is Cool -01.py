6787cc3e9a4e658081fa035776c2be473bf1d1d1
Ahimedzahir67 <kerons1234@gmail.com>
2026-01-08 03:02:59 +0300

    Update and rename Coding is Cool -01.py to exercise-01/Coding is Cool -01.py
    created coding is cool

--- Coding is Cool -01.py
+++
@@ -1,7 +0,0 @@
-print
-wordi = "Coding"
-word2 = "is"
-word3 = "COOL"
-
-
-# Use string concatenation to comdine the varibles and print the phrase  print(word1 + word2 + word3)
---
+++ exercise-01/Coding is Cool -01.py
@@ -0,0 +1,8 @@
+print
+wordi = "Coding"
+word2 = "is"
+word3 = "COOL"
+
+
+
+# Use string concatenation to comdine the varibles and print the phrase  print(word1 + word2 + word3)

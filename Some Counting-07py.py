916a14113fa8d9662edc65cb5d38b4eb3871358e
Ahimedzahir67 <kerons1234@gmail.com>
2026-01-08 03:36:18 +0300

    complet Some Counting-07py
    various for loop count logi

---
+++ Some Counting-07py.py
@@ -0,0 +1,15 @@
+# [span_24](start_span)Exercise 7: Using for loops for counting[span_24](end_span)
+print("0 to 50:")
+for i in range(51): print(i, end=" ")
+
+print("\n50 to 0:")
+for i in range(50, -1, -1): print(i, end=" ")
+
+print("\n30 to 50:")
+for i in range(30, 51): print(i, end=" ")
+
+print("\n50 down to 10 (step 2):")
+for i in range(50, 9, -2): print(i, end=" ")
+
+print("\n100 to 200 (step 5):")
+for i in range(100, 201, 5): print(i, end=" ")
